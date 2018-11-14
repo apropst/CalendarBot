efc992377e862d81d6cdcc7348752bfd458f97aa
AndrewP <apropst@gmail.com>
2018-11-14 01:06:11 -0500

    Iterated on multi-message handling.

--- CalendarBot/bot.js
+++ CalendarBot/bot.js
@@ -7,7 +7,7 @@ var sql = new SQLite('./db.sqlite');
 var client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('Logged in as ' + client.user.username + '!');
+	console.log('Logged in as ' + client.user.username + '!');
 	
 	var table = sql.prepare("SELECT COUNT(*) FROM sqlite_master WHERE type = 'table';").get();
 	
@@ -26,30 +26,50 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-	if (message.author.id == client.id)
+	if (message.author.bot)
 		return;
 	
-    if (message.content.substring(0, 1) == '!') {
-        var args = message.content.substring(1).split(' ');
-        var cmd = args[0];
-		var arg1 = args[1];
-		var arg2 = args[2];
-		var arg3 = args[3];
+	if (message.content.toLowerCase() == '!register') {
+		if (!userExists(message.author.id)) {
+			message.channel.send('<@'+ message.author.id + '> Please provide your timezone (Eastern, Central, Mountain, Pacific).');
+			
+			var collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
 		
-        args = args.splice(1);
-        switch(cmd) {
-            case 'ping':
+			collector.on('collect', message => {
+				if (message.content.toLowerCase() == 'eastern') {
+					var newUser = {
+						user: message.author.username,
+						userid: message.author.id,
+						timezone: 'Eastern'
+					}
+					client.setUser.run(newUser);
+					message.channel.send('<@'+ message.author.id + '> Added you to the database!');
+					collector.stop();
+				}
+			});
+		} else {
+			message.channel.send('<@'+ message.author.id + '> You\'re already in the database!');
+			return;
+		}
+	} else if (!userExists(message.author.id)) {
+		message.channel.send('<@'+ message.author.id + '> You don\'t exist in the database yet. Please use \'!Register\' to add yourself to the database!');
+		return;
+	} else if (message.content.substring(0, 1) == '!') {
+		var cmd = message.content.substring(1).toLowerCase();
+		
+		switch(cmd) {
+			case 'ping':
 				message.channel.send('<@'+ message.author.id + '> Pong! ' + message.author.username + ' ' + message.author.id);
-            break;
-            
+			break;
+			
 			case 'setevent':
 				if (arg1 != "" && arg2 != "" && arg3 != "") {
-					var newevent = {
+					var newEvent = {
 						name: arg1,
 						starttime: arg2,
 						endtime: arg3
 					}
-					client.setEvent.run(newevent);
+					client.setEvent.run(newEvent);
 				}
 			break;
 			
@@ -59,11 +79,11 @@ client.on('message', message => {
 					message.channel.send('Event ID: ' + eventdata.id + ' Event Name: ' + eventdata.name + ' Event Start Time: ' + eventdata.starttime + ' Event End Time: ' + eventdata.endtime);
 				}
 			break;
-        }
+		}
 		
 		var collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
-		console.log(collector);
-
+		console.log(collector._events.collect.toString());
+		
 		collector.on('collect', message => {
 			if (message.content == 'ping') {
 				message.channel.send('pong!');
@@ -73,3 +93,14 @@ client.on('message', message => {
 });
 
 client.login(auth.token);
+
+function userExists(id) {
+	if (client.getUser.get(id) == null)
+		return false;
+	else
+		return true;
+}
+
+function waitingForInput(id) {
+	
+}
