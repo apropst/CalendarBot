961594cd2d76c7f1b85ea240cce54fb0a72606b9
AndrewP <apropst@gmail.com>
2018-12-29 20:42:47 -0500

    Refined user adding and worked message collecting into more areas.

--- CalendarBot/bot.js
+++ CalendarBot/bot.js
@@ -6,7 +6,7 @@ var sql = new SQLite('./db.sqlite');
 
 var client = new Discord.Client();
 
-var createQueue = []
+var messageQueue = [];
 
 client.on('ready', () => {
 	console.log('Logged in as ' + client.user.username + '!');
@@ -15,93 +15,107 @@ client.on('ready', () => {
 	
 	if (!table['COUNT(*)']) {
 		sql.prepare("CREATE TABLE events (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, starttime INTEGER, endtime INTEGER);").run();
-		sql.prepare("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT, userid TEXT, timezone TEXT);").run();
+		sql.prepare("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, userid TEXT, timezone TEXT);").run();
 		sql.prepare("CREATE UNIQUE INDEX idx_events_id ON events (id);").run();
 		sql.pragma("synchronous = 1");
 		sql.pragma("journal_mode = wal");
 	}
 	
 	client.getEvent = sql.prepare("SELECT * FROM events WHERE name = ?");
-	client.setEvent = sql.prepare("INSERT OR REPLACE INTO events (name, starttime, endtime) VALUES (@name, @starttime, @endtime);");
+	client.addEvent = sql.prepare("INSERT OR REPLACE INTO events (name, starttime, endtime) VALUES (@name, @starttime, @endtime);");
 	client.getUser = sql.prepare("SELECT * FROM users WHERE userid = ?");
-	client.setUser = sql.prepare("INSERT OR REPLACE INTO users (user, userid, timezone) VALUES (@user, @userid, @timezone);");
+	client.addUser = sql.prepare("INSERT OR REPLACE INTO users (username, userid, timezone) VALUES (@username, @userid, @timezone);");
 });
 
 client.on('message', message => {
 	if (message.author.bot)
 		return;
 	
-	if (message.content.toLowerCase() == '!register' && !waitingForInput(message.author.id)) {
-		if (!userExists(message.author.id)) {
-			message.channel.send('<@'+ message.author.id + '> Please provide your timezone (Eastern, Central, Mountain, Pacific).');
+	if (!waitingForInput(message.author.id)) {
+		if (message.content.toLowerCase() == '!register') {
+			if (!userExists(message.author.id)) {
+				var collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id);
+				
+				messageQueue.push(message.author.id);
+				
+				message.channel.send('<@'+ message.author.id + '> Please provide your timezone (Eastern, Central, Mountain, Pacific).');
 			
-			var collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
-		
-			collector.on('collect', message => {
-				if (message.content.toLowerCase() == 'eastern') {
-					var newUser = {
-						user: message.author.username,
-						userid: message.author.id,
-						timezone: 'Eastern'
+				collector.on('collect', message => {
+					if (message.content.toLowerCase() == 'eastern' || message.content.toLowerCase() == 'central' || message.content.toLowerCase() == 'mountain' || message.content.toLowerCase() == 'pacific') {
+						var newUser = {
+							username: message.author.username,
+							userid: message.author.id,
+							timezone: message.content.toLowerCase().charAt(0).toUpperCase() + message.content.toLowerCase().slice(1)
+						}
+						client.addUser.run(newUser);
+						message.channel.send('<@'+ message.author.id + '> You\'ve been added to the database!');
+						collector.stop();
+					} else if (message.content.toLowerCase() == '!cancel') {
+						collector.stop();
+					} else {
+						message.channel.send('<@'+ message.author.id + '> That isn\'t a valid option. Please try again or type \'!Cancel\' to stop.');
 					}
-					client.setUser.run(newUser);
-					message.channel.send('<@'+ message.author.id + '> Added you to the database!');
-					collector.stop();
-				}
-			});
-		} else {
-			message.channel.send('<@'+ message.author.id + '> You\'re already in the database!');
+				});
+				
+				collector.on('end', (collection, reason) => {
+					messageQueue = arrRemove(messageQueue, message.author.id);
+				});
+			} else {
+				message.channel.send('<@'+ message.author.id + '> You\'re already in the database!');
+				return;
+			}
+		} else if (message.content.substring(0, 1) == '!' && !userExists(message.author.id)) {
+			message.channel.send('<@'+ message.author.id + '> You don\'t exist in the database yet. Please use \'!Register\' to add yourself to the database!');
 			return;
-		}
-	} else if (!userExists(message.author.id) && !waitingForInput(message.author.id)) {
-		message.channel.send('<@'+ message.author.id + '> You don\'t exist in the database yet. Please use \'!Register\' to add yourself to the database!');
-		return;
-	} else if (message.content.substring(0, 1) == '!' && !waitingForInput(message.author.id)) {
-		var cmd = message.content.substring(1).toLowerCase();
-		
-		switch(cmd) {
-			case 'ping':
-				message.channel.send('<@'+ message.author.id + '> Pong! ' + message.author.username + ' ' + message.author.id);
-			break;
+		} else if (message.content.substring(0, 1) == '!') {
+			var cmd = message.content.substring(1).toLowerCase();
 			
-			case 'setevent':
-				if (arg1 != "" && arg2 != "" && arg3 != "") {
-					var newEvent = {
-						name: arg1,
-						starttime: arg2,
-						endtime: arg3
+			switch(cmd) {
+				case 'ping':
+					message.channel.send('<@'+ message.author.id + '> Pong! ' + message.author.username + ' ' + message.author.id);
+					
+					var collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id);
+			
+					messageQueue.push(message.author.id);
+					
+					collector.on('collect', message => {
+						if (message.content == 'ping') {
+							message.channel.send('pong!');
+							collector.stop();
+						}
+					});
+					
+					collector.on('end', (collection, reason) => {
+						messageQueue = arrRemove(messageQueue, message.author.id);
+					});
+					break;
+				
+				case 'addevent':
+					if (arg1 != "" && arg2 != "" && arg3 != "") {
+						var newEvent = {
+							name: arg1,
+							starttime: arg2,
+							endtime: arg3
+						}
+						client.addEvent.run(newEvent);
 					}
-					client.setEvent.run(newEvent);
-				}
-			break;
-			
-			case 'getevent':
-				if (arg1 != "") {
-					var eventdata = client.getEvent.get(arg1);
-					message.channel.send('Event ID: ' + eventdata.id + ' Event Name: ' + eventdata.name + ' Event Start Time: ' + eventdata.starttime + ' Event End Time: ' + eventdata.endtime);
-				}
-			break;
+					break;
+				
+				case 'getevent':
+					if (arg1 != "") {
+						var eventdata = client.getEvent.get(arg1);
+						message.channel.send('Event ID: ' + eventdata.id + ' Event Name: ' + eventdata.name + ' Event Start Time: ' + eventdata.starttime + ' Event End Time: ' + eventdata.endtime);
+					}
+					break;
+				
+				case 'whoami':
+					var userData = client.getUser.get(message.author.id);
+					message.channel.send('User ID: ' + userData.id + ' Username: ' + userData.username + ' Discord User ID: ' + userData.userid + ' Timezone: ' + userData.timezone);
+					break;
+				
+			}
 		}
-		
-		var collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, {});
-		
-		createQueue.push(message.author.id);
-		
-		collector.on('collect', message => {
-			if (message.content == 'ping') {
-				message.channel.send('pong!');
-				collector.stop('This is a reason');
-			}
-		});
-		
-		collector.on('end', (collection, reason) => {
-			for (var x = 0; x < createQueue.length; x++) {
-				if (createQueue[x] === message.author.id) {
-					createQueue.splice(x, 1);
-				}
-			}
-		});
-	} else if (waitingForInput(message.author.id)) {
+	} else {
 		message.channel.send('<@'+ message.author.id + '> I am currently waiting for you to complete the current setup!');
 		return;
 	}
@@ -117,11 +131,21 @@ function userExists(id) {
 }
 
 function waitingForInput(id) {
-	for (var x = 0; x < createQueue.length; x++) {
-		if (createQueue[x] === id) {
+	for (var x = 0; x < messageQueue.length; x++) {
+		if (messageQueue[x] === id) {
 			return true;
 		}
 	}
 	
 	return false;
+}
+
+function arrRemove(arr, value) {
+	for (var x = 0; x < arr.length; x++) {
+		if (arr[x] === value) {
+			arr.splice(x, 1);
+		}
+	}
+	
+	return arr;
 }
