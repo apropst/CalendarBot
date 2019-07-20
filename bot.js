368e39409ff00a6b905f21fc15c4515ba4c7764b
Andrew Propst <apropst@gmail.com>
2019-07-20 15:07:16 -0400

    Added voice line playback and prod/test logic

--- bot.js
+++ bot.js
@@ -1,32 +1,32 @@
 /*
 To Do:
-- Test event deletion
 - Add message validation
 - Flesh out command list
    - Register for events (re-use existing !register command?)
-   - Add event deletion
    - Add event editing, how to handle alerting users who already registered?
+   - Allow requests for user availability
 - Apply 'AddEvent' message handling to all other commands where necessary
 - Add leading command character for all input(!)?
 - Recurring events?
 - Rework queueRemove to filter by message type
-
 */
 
-var Discord = require('discord.js');
-var logger = require('winston');
-var auth = require('./auth.json');
-var SQLite = require('better-sqlite3');
-var sql = new SQLite('./db.sqlite');
+const Discord = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const logger = require('winston');
+const auth = require('./auth.json');
+const SQLite = require('better-sqlite3');
+const sql = new SQLite('./db.sqlite');
 
-var client = new Discord.Client();
+const client = new Discord.Client();
 
 var messageQueue = [];
 
 client.on('ready', () => {
 	console.log('Logged in as ' + client.user.username + '!');
 	
-	var table = sql.prepare("SELECT COUNT(*) FROM sqlite_master WHERE type = 'table';").get();
+	const table = sql.prepare("SELECT COUNT(*) FROM sqlite_master WHERE type = 'table';").get();
 	
 	if (!table['COUNT(*)']) {
 		sql.prepare("CREATE TABLE events (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, creatorid TEXT, start TEXT, end TEXT);").run();
@@ -53,7 +53,7 @@ client.on('message', message => {
 		return;
 	
 	if (!waitingForInput(message.author.id)) {
-		if (message.content.toLowerCase() == '!register') {
+		if (message.content.toLowerCase() == '!register' && !IsProd()) {
 			if (!userExists(message.author.id)) {
 				var collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id);
 				
@@ -91,199 +91,219 @@ client.on('message', message => {
 				message.channel.send('<@'+ message.author.id + '> You\'re already in the database!');
 				return;
 			}
-		} else if (message.content.substring(0, 1) == '!' && !userExists(message.author.id)) {
+		} else if ((message.content.substring(0, 1) == '!' && !userExists(message.author.id)) && !IsProd()) {
 			message.channel.send('<@'+ message.author.id + '> You don\'t exist in the database yet. Please use \'!Register\' to add yourself to the database!');
 			return;
 		} else if (message.content.substring(0, 1) == '!') {
 			var cmd = message.content.substring(1).toLowerCase();
-			
-			switch(cmd) {
-				case 'ping':
-					message.channel.send('<@'+ message.author.id + '> Pong! ' + message.author.username + ' ' + message.author.id);
-					
-					var collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id);
-			
-					var queueObj = {
-						userid: message.author.id,
-						type: cmd,
-						step: 1
-					}
-					
-					messageQueue.push(queueObj);
-					
-					collector.on('collect', message => {
-						if (message.content == 'ping') {
-							message.channel.send('pong!');
-							collector.stop();
-						}
-					});
-					
-					collector.on('end', (collection, reason) => {
-						messageQueue = queueRemove(messageQueue, message.author.id);
-					});
-					break;
+			if (!IsProd()) {
+				switch(cmd) {
+					case 'ping':
+						message.channel.send('<@'+ message.author.id + '> Pong! ' + message.author.username + ' ' + message.author.id);
+						
+						var collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id);
 				
-				case 'addevent':
-					var collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id);
-					
-					var queueObj = {
-						userid: message.author.id,
-						type: cmd,
-						step: 1,
-						messages: []
-					}
-					
-					messageQueue.push(queueObj);
-					
-					message.channel.send('<@'+ message.author.id + '> Okay. What is the name of your event?');
-					
-					collector.on('collect', message => {
-						switch (getStep(message.author.id, 'addevent')) {
-							case 1:
-								queueAddMsg(message.author.id, 'addevent', message.content);
-								messageQueue = queueIncrement(messageQueue, message.author.id, 'addevent');
-								message.channel.send('<@'+ message.author.id + '> On which date will the event occur (MM/DD/YYYY)?');
-								break;
-								
-							case 2:
-								queueAddMsg(message.author.id, 'addevent', message.content);
-								messageQueue = queueIncrement(messageQueue, message.author.id, 'addevent');
-								message.channel.send('<@'+ message.author.id + '> What time will your event start (H:MM AM/PM)?');
-								break;
-								
-							case 3:
-								queueAddMsg(message.author.id, 'addevent', message.content);
-								message.channel.send('<@'+ message.author.id + '> What time will your event end (H:MM AM/PM)?');
-								messageQueue = queueIncrement(messageQueue, message.author.id, 'addevent');
-								break;
-								
-							case 4:
-								queueAddMsg(message.author.id, 'addevent', message.content);
+						var queueObj = {
+							userid: message.author.id,
+							type: cmd,
+							step: 1
+						}
+						
+						messageQueue.push(queueObj);
+						
+						collector.on('collect', message => {
+							if (message.content == 'ping') {
+								message.channel.send('pong!');
 								collector.stop();
-								break;
-						}
-					});
-					
-					collector.on('end', (collection, reason) => {
-						var messages = queueGetMsgs(message.author.id, 'addevent');
-						
-						var newEvent = {
-							name: messages[0],
-							creatorid: message.author.id,
-							start: new Date(messages[1] + ' ' + messages[2]).toISOString(),
-							end: new Date(messages[1] + ' ' + messages[3]).toISOString()
-						}
-						
-						client.addEvent.run(newEvent);
-						
-						messageQueue = queueRemove(messageQueue, message.author.id);
-						
-						message.channel.send('<@'+ message.author.id + '> Event \'' + newEvent.name + '\' added.');
-					});
-					
-					break;
-				
-				case 'deleteevent':
-					var collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id);
-					
-					var queueObj = {
-						userid: message.author.id,
-						type: cmd,
-						step: 1,
-						messages: []
-					}
-					
-					messageQueue.push(queueObj);
-					
-					message.channel.send('<@'+ message.author.id + '> Enter the name of the event you want to delete.');
-					
-					collector.on('collect', message => {
-						switch (getStep(message.author.id, cmd)) {
-							case 1:
-								queueAddMsg(message.author.id, cmd, message.content);
-								messageQueue = queueIncrement(messageQueue, message.author.id, cmd);
-								message.channel.send('<@'+ message.author.id + '> Are you sure you want to delete this event? Type "DELETE" to confirm or "CANCEL" to cancel.');
-								break;
-							
-							case 2:
-								queueAddMsg(message.author.id, cmd, message.content);
-								collector.stop();
-								break;
-						}
-					});
-					
-					collector.on('end', (collection, reason) => {
-						var messages = queueGetMsgs(message.author.id, cmd);
-						
-						if (messages[1].toLowerCase() == 'delete') {
-							var searchEvent = client.getEvent.get(messages[0]);
-							
-							if (typeof searchEvent !== 'undefined') {
-								deleteEvent(messages[0]);
-								message.channel.send('Event ' + messages[0] + ' deleted.');
-							} else {
-								message.channel.send('No event by name "' + messages[0] + '" found. Please try again.');
 							}
-						} else {
-							message.channel.send('Event deletion cancelled.');
+						});
+						
+						collector.on('end', (collection, reason) => {
+							messageQueue = queueRemove(messageQueue, message.author.id);
+						});
+						break;
+					
+					case 'addevent':
+						var collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id);
+						
+						var queueObj = {
+							userid: message.author.id,
+							type: cmd,
+							step: 1,
+							messages: []
 						}
 						
-						messageQueue = queueRemove(messageQueue, message.author.id);
-					});
-					break;
-				
-				case 'getevent':
-					var collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id);
-					
-					var queueObj = {
-						userid: message.author.id,
-						type: cmd,
-						step: 1
-					}
-					
-					messageQueue.push(queueObj);
-					
-					message.channel.send('<@'+ message.author.id + '> Enter the name of the event you want to search for.');
-					
-					collector.on('collect', message => {
-						switch (getStep(message.author.id, 'getevent')) {
-							case 1:
-								collector.stop();
-								break;
-						}
-					});
-					
-					collector.on('end', (collection, reason) => {
-						var eventResult = client.getEvent.get(collection.array()[0].content);
+						messageQueue.push(queueObj);
 						
-						messageQueue = queueRemove(messageQueue, message.author.id);
+						message.channel.send('<@'+ message.author.id + '> Okay. What is the name of your event?');
 						
-						if (typeof eventResult !== 'undefined') {
-							message.channel.send('Event ID: ' + eventResult.id + '\nEvent Name: ' + eventResult.name + '\nEvent Creator: ' + eventResult.creatorid + '\nEvent Start Time: ' + eventResult.start + '\nEvent End Time: ' + eventResult.end);
-						} else {
-							message.channel.send('No event with name found.');
+						collector.on('collect', message => {
+							switch (getStep(message.author.id, 'addevent')) {
+								case 1:
+									queueAddMsg(message.author.id, 'addevent', message.content);
+									messageQueue = queueIncrement(messageQueue, message.author.id, 'addevent');
+									message.channel.send('<@'+ message.author.id + '> On which date will the event occur (MM/DD/YYYY)?');
+									break;
+									
+								case 2:
+									queueAddMsg(message.author.id, 'addevent', message.content);
+									messageQueue = queueIncrement(messageQueue, message.author.id, 'addevent');
+									message.channel.send('<@'+ message.author.id + '> What time will your event start (H:MM AM/PM)?');
+									break;
+									
+								case 3:
+									queueAddMsg(message.author.id, 'addevent', message.content);
+									message.channel.send('<@'+ message.author.id + '> What time will your event end (H:MM AM/PM)?');
+									messageQueue = queueIncrement(messageQueue, message.author.id, 'addevent');
+									break;
+									
+								case 4:
+									queueAddMsg(message.author.id, 'addevent', message.content);
+									collector.stop();
+									break;
+							}
+						});
+						
+						collector.on('end', (collection, reason) => {
+							var messages = queueGetMsgs(message.author.id, 'addevent');
+							
+							var newEvent = {
+								name: messages[0],
+								creatorid: message.author.id,
+								start: new Date(messages[1] + ' ' + messages[2]).toISOString(),
+								end: new Date(messages[1] + ' ' + messages[3]).toISOString()
+							}
+							
+							client.addEvent.run(newEvent);
+							
+							messageQueue = queueRemove(messageQueue, message.author.id);
+							
+							message.channel.send('<@'+ message.author.id + '> Event \'' + newEvent.name + '\' added.');
+						});
+						
+						break;
+					
+					case 'deleteevent':
+						var collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id);
+						
+						var queueObj = {
+							userid: message.author.id,
+							type: cmd,
+							step: 1,
+							messages: []
 						}
-					});
+						
+						messageQueue.push(queueObj);
+						
+						message.channel.send('<@'+ message.author.id + '> Enter the name of the event you want to delete.');
+						
+						collector.on('collect', message => {
+							switch (getStep(message.author.id, cmd)) {
+								case 1:
+									queueAddMsg(message.author.id, cmd, message.content);
+									messageQueue = queueIncrement(messageQueue, message.author.id, cmd);
+									message.channel.send('<@'+ message.author.id + '> Are you sure you want to delete this event? Type "DELETE" to confirm or "CANCEL" to cancel.');
+									break;
+								
+								case 2:
+									queueAddMsg(message.author.id, cmd, message.content);
+									collector.stop();
+									break;
+							}
+						});
+						
+						collector.on('end', (collection, reason) => {
+							var messages = queueGetMsgs(message.author.id, cmd);
+							
+							if (messages[1].toLowerCase() == 'delete') {
+								var searchEvent = client.getEvent.get(messages[0]);
+								
+								if (typeof searchEvent !== 'undefined') {
+									deleteEvent(messages[0]);
+									message.channel.send('Event ' + messages[0] + ' deleted.');
+								} else {
+									message.channel.send('No event by name "' + messages[0] + '" found. Please try again.');
+								}
+							} else {
+								message.channel.send('Event devarion cancelled.');
+							}
+							
+							messageQueue = queueRemove(messageQueue, message.author.id);
+						});
+						break;
 					
-					break;
+					case 'getevent':
+						var collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id);
+						
+						var queueObj = {
+							userid: message.author.id,
+							type: cmd,
+							step: 1
+						}
+						
+						messageQueue.push(queueObj);
+						
+						message.channel.send('<@'+ message.author.id + '> Enter the name of the event you want to search for.');
+						
+						collector.on('collect', message => {
+							switch (getStep(message.author.id, 'getevent')) {
+								case 1:
+									collector.stop();
+									break;
+							}
+						});
+						
+						collector.on('end', (collection, reason) => {
+							var eventResult = client.getEvent.get(collection.array()[0].content);
+							
+							messageQueue = queueRemove(messageQueue, message.author.id);
+							
+							if (typeof eventResult !== 'undefined') {
+								message.channel.send('Event ID: ' + eventResult.id + '\nEvent Name: ' + eventResult.name + '\nEvent Creator: ' + eventResult.creatorid + '\nEvent Start Time: ' + eventResult.start + '\nEvent End Time: ' + eventResult.end);
+							} else {
+								message.channel.send('No event with name found.');
+							}
+						});
+						
+						break;
+						
+					case 'getevents':
+						var eventList = client.getEvents.all();
+	
+						if (eventList.length == 0)
+							message.channel.send('No events exist currently!');
+						
+						for (var x = 0; x < eventList.length; x++) {
+							message.channel.send('Event ID: ' + eventList[x].id + '\nEvent Name: ' + eventList[x].name + '\nEvent Creator: ' + eventList[x].creatorid + '\nEvent Start Time: ' + eventList[x].start + '\nEvent End Time: ' + eventList[x].end);
+						}
+						
+						break;
 					
-				case 'getevents':
-					var eventList = client.getEvents.all();
+					case 'whoami':
+						var userData = client.getUser.get(message.author.id);
+						message.channel.send('User ID: ' + userData.id + '\nUsername: ' + userData.username + '\nDiscord User ID: ' + userData.userid + '\nTimezone: ' + userData.timezone);
+						break;
 
-					if (eventList.length == 0)
-						message.channel.send('No events exist currently!');
+					case 'voicelines':
+						outputVoicelines(message);
+						break;
 					
-					for (var x = 0; x < eventList.length; x++) {
-						message.channel.send('Event ID: ' + eventList[x].id + '\nEvent Name: ' + eventList[x].name + '\nEvent Creator: ' + eventList[x].creatorid + '\nEvent Start Time: ' + eventList[x].start + '\nEvent End Time: ' + eventList[x].end);
-					}
-					
-					break;
-				
-				case 'whoami':
-					var userData = client.getUser.get(message.author.id);
-					message.channel.send('User ID: ' + userData.id + '\nUsername: ' + userData.username + '\nDiscord User ID: ' + userData.userid + '\nTimezone: ' + userData.timezone);
-					break;
-				
+					default:
+						playMusic(cmd, message);
+						break;
+				}
+			} else {
+				switch(cmd) {
+					/*
+					case 'voicelines':
+						outputVoicelines(message);
+						break;
+					*/
+
+					default:
+						playMusic(cmd, message);
+						break;
+				}
 			}
 		}
 	} /*else {
@@ -293,6 +313,13 @@ client.on('message', message => {
 });
 
 client.login(auth.token);
+
+function IsProd() {
+	if (auth.token.substring(0,3) == "NTk")
+		return true;
+	
+	return false;
+}
 
 function userExists(id) {
 	if (client.getUser.get(id) == null)
@@ -360,4 +387,90 @@ function queueGetMsgs(id, type) {
 function deleteEvent(name) {
 	sql.exec('DELETE FROM events WHERE name = "' + name + '"');
 	sql.exec('DELETE FROM eventregistration WHERE event = "' + name + '"');
+}
+
+function getFiles(dir, done) {
+    let results = [];
+
+    fs.readdir(dir, function(err, list) {
+        if (err) return done(err);
+
+        var pending = list.length;
+
+        if (!pending) return done(null, results);
+
+        list.forEach(function(file){
+            file = path.resolve(dir, file);
+
+            fs.stat(file, function(err, stat){
+                if (stat && stat.isDirectory()) {
+                    getFiles(file, function(err, res){
+                        results = results.concat(res);
+                        if (!--pending) done(null, results);
+                    });
+                } else {
+                    results.push(file);
+
+                    if (!--pending) done(null, results);
+                }
+            });
+        });
+    });
+};
+
+function outputVoicelines(message) {
+	getFiles('media', function(err, data) {
+		if (err) {
+			console.log('Error: ' + err);
+		}
+
+		let output = new String();
+
+		output += 'Available voicelines:';
+
+		data.forEach(function(file){
+			let fileName = file.substring(file.lastIndexOf("\\") + 1);
+			let truncName = file.slice(0,file.length - fileName.length - 1);
+			let heroName = truncName.substring(truncName.lastIndexOf("\\") + 1);
+
+			output += '\n' + heroName + ' - ' + fileName.slice(0, fileName.length - 4);
+		});
+
+		console.log(output);
+
+		message.channel.send(output);
+	});
+}
+
+
+const playMusic = async (cmd, message) => {
+	const vChannel = message.member.voice.channel;
+
+	getFiles('media', async (err, data) => {
+		if (err) {
+			console.log('Error: ' + err);
+		}
+		
+		for (let file of data) {
+			let fileName = file.substring(file.lastIndexOf("\\") + 1);
+			let truncName = file.slice(0,file.length - fileName.length - 1);
+			let heroName = truncName.substring(truncName.lastIndexOf("\\") + 1);
+			let commandName = fileName.slice(0, fileName.length - 4);
+	
+			if (cmd == commandName) {
+				if (vChannel) {
+					await vChannel.join().then(connection => {
+						dispatcher = connection.play('media/' + heroName + '/' + cmd + '.mp3');
+						dispatcher.on("end", end => {
+							vChannel.leave();
+						});
+					}).catch(error => {
+						message.channel.send("Error playing file: " + error);
+					});
+				} else {
+					message.channel.send(new Discord.MessageAttachment('media/' + heroName + '/' + cmd + '.mp3', cmd + '.mp3'));
+				}
+			}
+		}
+	});
 }
